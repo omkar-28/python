0a6ce81172c9d1f8b7ee30cb14c0c0f75eede8bd
omkar-28 <37948343+omkar-28@users.noreply.github.com>
2019-02-15 16:20:54 +0530

    python files

---
+++ function1.py
@@ -0,0 +1,12 @@
+def hello_world():
+    print("Hello world, Welcome to python programming. \n")
+
+hello_world()
+
+def say_hello(name):
+    print("hello",name)
+
+name=input("Enter your name ")
+print("Your name is {}".format(name))
+say_hello(name)
+say_hello(name)
